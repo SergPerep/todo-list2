025d63842bfda79e725d269be33d9959808b8b45
Sergei Perepelkin <perepelkin.serge@gmail.com>
2021-12-17 16:19:41 +0100

    fixing

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors())
-// app.use(express.json()); // parse req.body as json
+app.use(express.json()); // parse req.body as json
 
 app.use(express.static(path.join(__dirname, "client/build")));
 
